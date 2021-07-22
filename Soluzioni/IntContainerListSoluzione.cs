9e435a3275bb2f7429ed56b52e12c4a15a87a53c
C Software <C-soft@edu-il.it>
2021-07-22 11:32:15 +0200

    Aggiunte soluzioni usando le liste

---
+++ Soluzioni/IntContainerListSoluzione.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contenitore
+{
+    class IntContainerListSoluzione
+    {
+        private readonly static int LIST_SIZE = 5;
+        private List<int> list;
+        public IntContainerListSoluzione()
+        {
+            list = new List<int>();
+        }
+
+        internal void InserisciElemento(int number)
+        {
+            if (list.Count > LIST_SIZE)
+                list.RemoveAt(0);
+
+            list.Add(number);
+        }
+        internal void StampaContenuto()
+        {
+            var i = 0;
+           foreach(var e in list)
+            {
+                Console.WriteLine($"{i++} - {e}");
+            }
+        }
+
+
+    }
+}
